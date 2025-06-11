2bfd8f2cd911d49eae0c2eeeff6724d4c7c2a375
DiegoA-Utp <u20218230@utp.edu.pe>
2025-06-11 17:21:48 -0500

    feat: crear el service implementado del Producto

---
+++ src/main/java/com/gitrebase/gitrebasepc1/Services/impl/ProductoServiceImpl.java
@@ -0,0 +1,46 @@
+package com.gitrebase.gitrebasepc1.Services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gitrebase.gitrebasepc1.Models.dao.IProductoDao;
+import com.gitrebase.gitrebasepc1.Models.entities.Producto;
+import com.gitrebase.gitrebasepc1.Services.IProductoService;
+
+import java.util.ArrayList;
+
+@Service
+
+public class ProductoServiceImpl implements IProductoService {
+
+    @Autowired
+    private IProductoDao productoDao;
+
+
+    @Override
+    public List<Producto> getAll() {
+
+        List<Producto> productos = new ArrayList<Producto>();
+        productoDao.findAll().forEach(productos::add);
+        return productos;
+    }
+
+    @Override
+    public Producto save(Producto producto) {
+        return productoDao.save(producto);
+    }
+
+    @Override
+    public Producto findById(Long id) {
+        return productoDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(Producto producto) {
+        productoDao.delete(producto);
+
+    }
+
+}
