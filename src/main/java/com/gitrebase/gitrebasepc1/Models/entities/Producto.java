eceb45edb2c525983e3145ec8d519f3dc6c73d1d
DiegoA-Utp <u20218230@utp.edu.pe>
2025-06-11 16:19:23 -0500

    feat: crear la entidad del Producto

---
+++ src/main/java/com/gitrebase/gitrebasepc1/Models/entities/Producto.java
@@ -0,0 +1,27 @@
+package com.gitrebase.gitrebasepc1.Models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int stock;
+}
