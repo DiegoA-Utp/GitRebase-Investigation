5ccd2e46a7b29fee2bb9726b93e50b77ba61cd2a
DiegoA-Utp <u20218230@utp.edu.pe>
2025-06-11 15:06:21 -0500

    first commit

---
+++ src/main/java/com/gitrebase/gitrebasepc1/Gitrebasepc1Application.java
@@ -0,0 +1,13 @@
+package com.gitrebase.gitrebasepc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitrebasepc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitrebasepc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitrebase/gitrebasepc1/Gitrebasepc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitrebase.gitrebasepc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitrebasepc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
