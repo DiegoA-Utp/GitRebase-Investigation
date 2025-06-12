74a0cc82a580686a488e7327ec4efcab232391fb
DiegoA-Utp <u20218230@utp.edu.pe>
2025-06-11 20:07:49 -0500

    fix: ICategoriaDao

--- src/main/java/com/gitrebase/gitrebasepc1/Models/dao/ICategoriaDao.java
+++ src/main/java/com/gitrebase/gitrebasepc1/Models/dao/ICategoriaDao.java
@@ -1,5 +1,9 @@
 package com.gitrebase.gitrebasepc1.Models.dao;
 
-public interface ICategoriaDao {
+import org.springframework.data.repository.CrudRepository;
 
+import com.gitrebase.gitrebasepc1.Models.entities.Categoria;
+
+public interface ICategoriaDao extends CrudRepository<Categoria, Long> {
+    
 }
