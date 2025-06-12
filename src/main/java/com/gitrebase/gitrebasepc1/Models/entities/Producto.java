1c84a3d44a93b1de31317093fcdf30b256e73fdb
DiegoA-Utp <u20218230@utp.edu.pe>
2025-06-12 10:51:49 -0500

    feat: se agrego la relacion de producto con categorio

--- src/main/java/com/gitrebase/gitrebasepc1/Models/entities/Producto.java
+++ src/main/java/com/gitrebase/gitrebasepc1/Models/entities/Producto.java
@@ -1,9 +1,13 @@
 package com.gitrebase.gitrebasepc1.Models.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,4 +28,7 @@ public class Producto {
     private String descripcion;
     private double precio;
     private int stock;
+    @JsonIgnoreProperties(value = { "productos", "hibernateLazyInitializer", "handler" }, allowSetters = true)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Categoria categoria;
 }
