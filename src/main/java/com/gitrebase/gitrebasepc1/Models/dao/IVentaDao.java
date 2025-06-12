9da1dcf4ee8a201bf014a5dcab5ef75296be4db2
Oswaldo Cabrejos Ronceros <U22209742@utp.edu.pe>
2025-06-12 10:41:55 -0500

    feat:Creacion de IVentaDao

---
+++ src/main/java/com/gitrebase/gitrebasepc1/Models/dao/IVentaDao.java
@@ -0,0 +1,7 @@
+package com.gitrebase.gitrebasepc1.Models.dao;
+
+import com.gitrebase.gitrebasepc1.Models.entities.Venta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IVentaDao extends CrudRepository<Venta,Long> {
+}
