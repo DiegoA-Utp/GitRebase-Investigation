06e23573475a882dde2a53d675b7745b5bdb2c6d
CarlosHuamanM <thecarlox2000@gmail.com>
2025-06-18 00:33:44 -0500

    Feat: Creacion de la entidad Sucursal

---
+++ src/main/java/com/gitrebase/gitrebasepc1/Models/entities/Surcursal.java
@@ -0,0 +1,28 @@
+package com.gitrebase.gitrebasepc1.Models.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Surcursal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    
+}
